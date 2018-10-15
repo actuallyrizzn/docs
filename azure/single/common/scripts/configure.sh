da5ae5d95ddec495fbc338fb2e054b662312bd41
r-kononov <rkononov@gochain.io>
2018-10-15 21:42:32 +0600

    updated azure template

--- azure/single/common/scripts/configure.sh
+++ azure/single/common/scripts/configure.sh
@@ -82,6 +82,18 @@ ACCOUNT_ID=$(sudo docker run -v $PWD:/root gochain/gochain gochain --datadir /ro
 echo "GOCHAIN_ACCT=0x$ACCOUNT_ID" > $HOMEDIR/.env
 echo "GOCHAIN_NETWORK=$NETWORK_ID" >> $HOMEDIR/.env
 
+###########################
+# Share configs
+###########################
+
+touch $HOMEDIR/shared/${ACCOUNT_ID}.account
+echo "console.log(admin.nodeInfo.enode);" > $HOMEDIR/enode.js
+docker run -v ${$HOMEDIR}:${$HOMEDIR} gochain/gochain --datadir ${$HOMEDIR}/node js ${$HOMEDIR}/enode.js
+
+###########################
+# Generate genesis
+###########################
+
 sed -i "s/#NETWORKID/$NETWORK_ID/g" $HOMEDIR/genesis || exit 1;
 sed -i "s/#CURRENTTSHEX/$CURRENT_TS_HEX/g" $HOMEDIR/genesis || exit 1;
 sed -i "s/#SIGNERADDRESS/$ACCOUNT_ID/g" $HOMEDIR/genesis || exit 1;
@@ -91,9 +103,9 @@ sed -i "s/#HEX/$INITIAL_BALANCE_HEX/g" $HOMEDIR/genesis || exit 1;
 
 mv $HOMEDIR/genesis $HOMEDIR/genesis.json
 
-sudo sudo rm -rf $PWD/node/GoChain
-sudo docker run --rm -v $PWD:/gochain -w /gochain gochain/gochain gochain --datadir /gochain/node init genesis.json
-#########################################
-# Install docker image from private repo
-#########################################
-sudo docker-compose up -d
+# sudo sudo rm -rf $PWD/node/GoChain
+# sudo docker run --rm -v $PWD:/gochain -w /gochain gochain/gochain gochain --datadir /gochain/node init genesis.json
+# #########################################
+# # Install docker image from private repo
+# #########################################
+# sudo docker-compose up -d
